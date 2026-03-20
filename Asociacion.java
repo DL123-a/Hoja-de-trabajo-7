6c526fd1e952c13f5162df02f6c58c096ef668dc
DL123-a <dannylealgt@gmail.com>
2026-03-20 00:22:15 -0600

    Implementacion de asociacion y main

--- Asociacion.java
+++ Asociacion.java
@@ -1,3 +1,28 @@
-public class Asociacion {
-    
+public class Asociacion<K extends Comparable<K>, V> implements Comparable<Asociacion<K, V>> {
+
+    private K clave;
+    private V valor;
+
+    public Asociacion(K clave, V valor) {
+        this.clave = clave;
+        this.valor = valor;
+    }
+
+    public K getClave() {
+        return clave;
+    }
+
+    public V getValor() {
+        return valor;
+    }
+
+    @Override
+    public int compareTo(Asociacion<K, V> otra) {
+        return this.clave.compareTo(otra.clave);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + clave + ", " + valor + ")";
+    }
 }
--- Main.java
+++ Main.java
@@ -1,2 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) {
+
+        
+        Arbol<Asociacion<String, String>> arbol = new Arbol<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("diccionario.txt"));
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+
+                
+                linea = linea.replace("(", "").replace(")", "");
+
+                
+                String[] partes = linea.split(",");
+
+                
+                String ingles = partes[0].trim();
+                String espanol = partes[1].trim();
+
+                
+                arbol.insertar(new Asociacion<>(ingles, espanol));
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo");
+            e.printStackTrace();
+        }
+
+        
+        System.out.println("Diccionario ordenado:");
+        arbol.inOrder();
+    }
+}
     
